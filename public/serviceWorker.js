3056913f2697c56defc671d15c1e33436442fbe6
ki-ask <site@kiask.xyz>
2023-06-25 02:47:48 +0600

    m

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,54 +1,15 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+const CHATGPT_CACHE = "chatgpt-cache";
 
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener("activate", function (event) {
+  console.log("ServiceWorker activated.");
 });
 
-self.addEventListener('install', async (event) => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CHATGPT_CACHE).then(function (cache) {
+      return cache.addAll([]);
+    }),
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+self.addEventListener("fetch", (e) => {});
