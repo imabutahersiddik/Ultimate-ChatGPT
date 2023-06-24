7e3775c7fa731728cbad32d9b26b879bd2ad87f6
ki-ask <site@kiask.xyz>
2023-06-25 05:34:29 +0600

    m

--- app/locales/tg.ts
+++ app/locales/tg.ts
@@ -72,15 +72,15 @@ const tg: RequiredLocaleType = {
       SubTitle: "I-export ang mga konteksto ng prompt",
     },
     Steps: {
-      Pagpili: "Pumili",
+      Select: "Pumili",
       Preview: "Preview",
     },
   },
-  Pumili: {
+  Select: {
     Search: "Maghanap",
-    Lahat: "Pumili ng Lahat",
-    Pinakabagong: "Pumili ng Pinakabagong",
-    Linawin: "Linisin",
+    All: "Pumili ng Lahat",
+    Latest: "Pumili ng Pinakabagong",
+    Clear: "Linisin",
   },
   Memory: {
     Title: "Memory Prompt",
@@ -103,99 +103,99 @@ const tg: RequiredLocaleType = {
     Actions: {
       ClearAll: "Burahin ang Lahat ng Data",
       ResetAll: "I-reset ang Lahat ng mga Setting",
-      Isara: "Isara",
-      KumpirmahinAngResetAll: "Sigurado ka bang nais mong i-reset ang lahat ng mga konfigurasyon?",
-      KumpirmahinAngClearAll: "Sigurado ka bang nais mong i-reset ang lahat ng data?",
+      Close: "Isara",
+      ConfirmResetAll: "Sigurado ka bang nais mong i-reset ang lahat ng mga konfigurasyon?",
+      ConfirmClearAll: "Sigurado ka bang nais mong i-reset ang lahat ng data?",
     },
-    Wikang: {
-      Pangalan: "Wikang", // ATTENTION: kung nais mong magdagdag ng bagong pagsasalin, mangyaring huwag isalin ang halagang ito, iiwan ito bilang `Language`
-      Lahat: "Lahat ng Mga Wikang",
+    Lang: {
+      Name: "Wikang", // ATTENTION: kung nais mong magdagdag ng bagong pagsasalin, mangyaring huwag isalin ang halagang ito, iiwan ito bilang `Language`
+      All: "Lahat ng Mga Wikang",
     },
     Avatar: "Avatar",
-    LakiNgFont: {
+    FontSize: {
       Title: "Laki ng Font",
       SubTitle: "I-adjust ang laki ng font ng laman ng chat",
     },
     Update: {
-      Bersyon: (x: string) => `Bersyon: ${x}`,
-      Pinakabagong: "Pinakabagong bersyon",
-      SuriinAngUpdate: "Suriin ang Update",
-      KasalukuyangSinusuri: "Sinusuri ang update...",
-      NatagpuangUpdate: (x: string) => `Natagpuan ang bagong bersyon: ${x}`,
-      PumuntaSaUpdate: "I-update",
+      Version: (x: string) => `Bersyon: ${x}`,
+      IsLatest: "Pinakabagong bersyon",
+      CheckUpdate: "Suriin ang Update",
+      IsChecking: "Sinusuri ang update...",
+      FoundUpdate: (x: string) => `Natagpuan ang bagong bersyon: ${x}`,
+      GoToUpdate: "I-update",
     },
     SendKey: "Ipadala ang Key",
-    Tema: "Tema",
+    Theme: "Tema",
     TightBorder: "Mahigpit na Border",
     SendPreviewBubble: {
       Title: "Ipadala ang Preview Bubble",
       SubTitle: "I-preview ang markdown sa isang bubble",
     },
-    Maskara: {
-      Pamagat: "Prompt Splash Screen",
+    Mask: {
+      Prompt: "Prompt Splash Screen",
       SubTitle: "Itakda ang isang prompt splash screen bago mag-umpisa ng bagong chat",
     },
     Prompt: {
-      HindiGumagana: {
-        Pamagat: "Huwag paganahin ang auto-kumpletong",
+      Disable: {
+        Title: "Huwag paganahin ang auto-kumpletong",
         SubTitle: "Maglagay ng / upang paganahin ang auto-kumpletong",
       },
-      Listahan: "Listahan ng Prompt",
-      BilangSaListahan: (builtin: number, custom: number) =>
+      List: "Listahan ng Prompt",
+      ListCount: (builtin: number, custom: number) =>
         `${builtin} built-in, ${custom} user-defined`,
-      I-edit: "I-edit",
+      Edit: "I-edit",
       Modal: {
-        Pamagat: "Listahan ng Prompt",
+        Title: "Listahan ng Prompt",
         Magdagdag: "Magdagdag ng Isa",
         Maghanap: "Maghanap ng Mga Prompt",
       },
       EditModal: {
-        Pamagat: "I-edit ang Prompt",
+        Title: "I-edit ang Prompt",
       },
     },
-    BilangNgKasaysayan: {
-      Pamagat: "Bilang ng Mga Nakalakip na Mensahe",
+    HistoryCount: {
+      Title: "Bilang ng Mga Nakalakip na Mensahe",
       SubTitle: "Bilang ng mga mensaheng ipinadala na kalakip bawat kahilingan",
     },
     CompressThreshold: {
-      Pamagat: "Bilang ng Paanurin ng Kasaysayan",
+      Title: "Bilang ng Paanurin ng Kasaysayan",
       SubTitle:
         "I-aapakan kung ang haba ng mga hindi pa naaangkat na mensahe ay lumalampas sa halaga",
     },
     Token: {
-      Pamagat: "API Key",
+      Title: "API Key",
       SubTitle: "Gamitin ang iyong key upang balewalain ang limitasyon ng access code",
       Placeholder: "OpenAI API Key",
     },
-    Paggamit: {
-      Pamagat: "Kalagayan ng Account",
+    Usage: {
+      Title: "Kalagayan ng Account",
       SubTitle(nagamit: any, kabuuan: any) {
-        return `Nagamit sa buwang ito $${nagamit}, pag-aari $${kabuuan}`;
+        return `Nagamit sa buwang ito $${used}, pag-aari $${total}`;
       },
-      KasalukuyangSinusuri: "Sinusuri...",
-      Suriin: "Suriin",
-      WalangAccess: "Maglagay ng API Key upang suriin ang balanse",
+      IsChecking: "Sinusuri...",
+      Check: "Suriin",
+      NoAccess: "Maglagay ng API Key upang suriin ang balanse",
     },
     AccessCode: {
-      Pamagat: "Access Code",
+      Title: "Access Code",
       SubTitle: "Pinagana ang access control",
       Placeholder: "Kailangan ng Access Code",
     },
     Endpoint: {
-      Pamagat: "Endpoint",
+      Title: "Endpoint",
       SubTitle: "Ang pasadyang endpoint ay dapat magsimula sa http(s)://",
     },
     Model: "Modelo",
     Temperature: {
-      Pamagat: "Temperature",
+      Title: "Temperature",
       SubTitle: "Ang mas malaking halaga ay gumagawa ng mas random na output",
     },
     MaxTokens: {
-      Pamagat: "Max na Bikitan",
+      Title: "Max na Bikitan",
       SubTitle: "Maximum na haba ng mga input token at mga ginawang token",
     },
     PresencePenalty: {
-      Pamagat: "Halaga ng Penalty sa Presence",
+      Title: "Halaga ng Penalty sa Presence",
       SubTitle:
         "Ang mas malaking halaga ay nagpapataas ng posibilidad na pag-usapan ang mga bagong paksa",
     },
@@ -205,80 +205,80 @@ const tg: RequiredLocaleType = {
     BotHello: "Hello! Paano kita matutulungan ngayon?",
     Error: "Mayroong nabaluktot, subukan muli mamayang hapon.",
     Prompt: {
-      Kasaysayan: (content: string) =>
+      History: (content: string) =>
         "Ito ay isang buod ng kasaysayan ng chat bilang pagsusuri: " + content,
-      Tema:
+      Topic:
         "Mangyaring lumikha ng isang four to five word na pamagat na inililista ang aming usapan nang walang anumang paghahain, bantas, tanda ng panipi, punto, simbolo, o karagdagang teksto. Alisin ang nagbibigkis na tanda ng panipi.",
-      Buod:
+      Summarize:
         "Suriin nang maikli ang pag-uusap sa 200 na salita o mas mababa na gagamitin bilang prompt para sa hinaharap na konteksto.",
     },
   },
   Copy: {
-    Tagumpay: "Nakopya sa clipboard",
-    Nabigo: "Hindi nagawa ang pagkopya, mangyaring bigyan ng pahintulot ang access sa clipboard",
+    Success: "Nakopya sa clipboard",
+    Failed: "Hindi nagawa ang pagkopya, mangyaring bigyan ng pahintulot ang access sa clipboard",
   },
-  Konteksto: {
-    Tostada: (x: any) => `Mayroong ${x} mga contextual prompt`,
-    I-edit: "Mga Contextual at Memory Prompts",
-    Idagdag: "Idagdag ang Prompt",
-    Linawin: "Inalis ang Konteksto",
-    Bumalik: "Bumalik",
+  Context: {
+    Toast: (x: any) => `Mayroong ${x} mga contextual prompt`,
+    Edit: "Mga Contextual at Memory Prompts",
+    Add: "Idagdag ang Prompt",
+    Clear: "Inalis ang Konteksto",
+    Revert: "Bumalik",
   },
   Plugin: {
-    Pangalan: "Plugin",
+    Name: "Plugin",
   },
-  Maskara: {
-    Pangalan: "Prompt",
-    Pahina: {
-      Pamagat: "Prompt template",
+  Mask: {
+    Name: "Prompt",
+    Page: {
+      Title: "Prompt template",
       SubTitle: (count: number) => `${count} mga prompt template`,
-      Paghahanap: "Paghahanap ng mga Template",
-      Lumikha: "Lumikha",
+      Search: "Paghahanap ng mga Template",
+      Create: "Lumikha",
     },
     Item: {
-      Impormasyon: (count: number) => `${count} mga prompt`,
+      Info: (count: number) => `${count} mga prompt`,
       Chat: "Chat",
-      Tingnan: "Tingnan",
-      I-edit: "I-edit",
-      Burahin: "Burahin",
-      KumpirmahinAngBurahin: "Kumpirmahin ang pagbura?",
+      View: "Tingnan",
+      Edit: "I-edit",
+      Delete: "Burahin",
+      DeleteConfirm: "Kumpirmahin ang pagbura?",
     },
     EditModal: {
-      Pamagat: (readonly: boolean) =>
+      Title: (readonly: boolean) =>
         `I-edit ang Prompt Template ${readonly ? "(readonly)" : ""}`,
-      I-download: "I-download",
-      Kopyahin: "Kopyahin",
+      Download: "I-download",
+      Clone: "Kopyahin",
     },
     Config: {
       Avatar: "Avatar ng Bot",
-      Pangalan: "Pangalan ng Bot",
+      Name: "Pangalan ng Bot",
       Sync: {
-        Pamagat: "Gamitin ang Global na Config",
+        Title: "Gamitin ang Global na Config",
         SubTitle: "Gamitin ang global config sa chat na ito",
-        Kumpirmahin: "Kumpirmahin ang pag-override ng custom config sa global config?",
+        Confirm: "Kumpirmahin ang pag-override ng custom config sa global config?",
       },
-      ItagoAngKonteksto: {
-        Pamagat: "Itago ang Context Prompts",
+      HideContext: {
+        Title: "Itago ang Context Prompts",
         SubTitle: "Huwag ipakita ang mga in-context na prompt sa chat",
       },
     },
   },
-  BagoChat: {
-    Bumalik: "Bumalik",
-    Lampas: "Simulan na",
-    Pamagat: "Pumili ng Prompt",
+  NewChat: {
+    Return: "Bumalik",
+    Skip: "Simulan na",
+    Title: "Pumili ng Prompt",
     SubTitle: "Makipag-chat sa Kaluluwa sa Likod ng Prompt",
-    HigitPa: "Maghanap ng Iba pa",
-    HindiIpakita: "Huwag Ipakita Muli",
-    KumpirmahinNaHindiIpakita: "Kumpirmahin ang hindi pagpapakita? Pwede mong paganahin ito sa mga setting mamaya.",
+    More: "Maghanap ng Iba pa",
+    NotShow: "Huwag Ipakita Muli",
+    ConfirmNoShow: "Kumpirmahin ang hindi pagpapakita? Pwede mong paganahin ito sa mga setting mamaya.",
   },
 
   UI: {
-    Kumpirmahin: "Kumpirmahin",
-    Kanselahin: "Kanselahin",
-    Isara: "Isara",
-    Lumikha: "Lumikha",
-    I-edit: "I-edit",
+    Confirm: "Kumpirmahin",
+    Cancel: "Kanselahin",
+    Close: "Isara",
+    Create: "Lumikha",
+    Edit: "I-edit",
   },
 };
 
