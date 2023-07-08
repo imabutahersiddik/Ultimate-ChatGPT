b9ca5b79ecaf0bad680c31e74de5bb8159417167
ki-ask <site@kiask.xyz>
2023-07-08 19:07:22 +0600

    M

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Ultimate
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Unlock The Ultimate Power of ChatGPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
