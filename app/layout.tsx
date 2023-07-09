27419290465e5ffb858e13e56831006e65f324f4
ki-ask <site@kiask.xyz>
2023-07-09 23:48:33 +0600

    Updated meta fields

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-page-custom-font */
+const FAVICON_FOLDER = "/favicons";
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
@@ -7,6 +8,9 @@ import { getClientConfig } from "./config/client";
 export const metadata = {
   title: "The Ultimate ChatGPT",
   description: "Experience the Ultimate ChatGPT Chat Bot: Powered by GPT-3.5 Turbo AI, our cutting-edge virtual assistant revolutionizes your online interactions. Engage in captivating conversations, get instant answers, and access a wealth of knowledge. With unparalleled intelligence and lightning-fast response times, our Chat Bot is your go-to source for expert advice, innovative ideas, and friendly banter. Don't miss out on this incredible opportunity to explore the future of AI. Click now to unlock endless possibilities with our advanced ChatGPT Chat Bot!",
+  image = "https://chatgpt.kiask.xyz/thumbnail.png",
+  imageAlt = "Ultimate ChatGPT Image",
+  canonical = "https://chatgpt.kiask.xyz",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -31,6 +35,38 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${FAVICON_FOLDER}/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={`${FAVICON_FOLDER}/favicon-32x32.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`${FAVICON_FOLDER}/favicon-16x16.png`}
+      />
+        <meta itemProp="image" content={image} />
+        <meta property="og:logo" content="https://chatgpt.kiask.xyz/logo.png"></meta>
+        <meta property="og:title" content={title} />
+        <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={imageAlt} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@Vercel" />
+      <meta name="twitter:creator" content="@dom__inic" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta property="twitter:image:alt" content={imageAlt} />
+      <meta name="twitter:player" content={canonical + "?card=1"} />
+      <meta name="twitter:player:width" content="300" />
+      <meta name="twitter:player:height" content="533" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
         <script async src="https://www.kiask.xyz/ad-anchor.js" />
