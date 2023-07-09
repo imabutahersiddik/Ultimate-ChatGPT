f40fdb946e43574cb0e305814e21540e1dbd544e
ki-ask <site@kiask.xyz>
2023-07-10 01:45:26 +0600

    Fixed bug of meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ import { getClientConfig } from "./config/client";
 export const metadata = {
   title: "The Ultimate ChatGPT",
   description: "Experience the Ultimate ChatGPT Chat Bot: Powered by GPT-3.5 Turbo AI, our cutting-edge virtual assistant revolutionizes your online interactions. Engage in captivating conversations, get instant answers, and access a wealth of knowledge. With unparalleled intelligence and lightning-fast response times, our Chat Bot is your go-to source for expert advice, innovative ideas, and friendly banter. Don't miss out on this incredible opportunity to explore the future of AI. Click now to unlock endless possibilities with our advanced ChatGPT Chat Bot!",
-  image = "/thumbnail.png",
-  imageAlt = "Ultimate ChatGPT Image",
   canonical = "https://chatgpt.kiask.xyz",
   viewport: {
     width: "device-width",
@@ -52,18 +50,18 @@ export default function RootLayout({
         sizes="16x16"
         href={`${FAVICON_FOLDER}/favicon-16x16.png`}
       />
-        <meta itemProp="image" content={image} />
+        <meta itemProp="image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
         <meta property="og:logo" content="https://chatgpt.kiask.xyz/logo.png"></meta>
         <meta property="og:title" content={title} />
-        <meta property="og:image" content={image} />
-      <meta property="og:image:alt" content={imageAlt} />
+        <meta property="og:image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
+      <meta property="og:image:alt" content="Ultimate ChatGPT Image" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@Vercel" />
       <meta name="twitter:creator" content="@dom__inic" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-      <meta property="twitter:image:alt" content={imageAlt} />
+      <meta name="twitter:image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
+      <meta property="twitter:image:alt" content="Ultimate ChatGPT Image" />
       <meta name="twitter:player" content={canonical + "?card=1"} />
       <meta name="twitter:player:width" content="300" />
       <meta name="twitter:player:height" content="533" />
