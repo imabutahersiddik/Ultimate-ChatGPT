6cf04234170754d232b5aaad1ec8aa1f10b5ff7e
ki-ask <site@kiask.xyz>
2023-07-10 01:55:11 +0600

    Fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,6 @@ export const metadata = {
     title: "The Ultimate ChatGPT - Unlock the ultimate features of ChatGPT",
     statusBarStyle: "default",
   },
-}: {
-  title?: string;
-  description?: string;
 };
 
 export default function RootLayout({
@@ -31,42 +28,45 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { title, description } = metadata; // Add these variables or assign appropriate values
+  const canonical = "https://chatgpt.kiask.xyz"; // Add the canonical URL or assign an appropriate value
+
   return (
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`${FAVICON_FOLDER}/apple-touch-icon.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${FAVICON_FOLDER}/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${FAVICON_FOLDER}/favicon-16x16.png`}
-      />
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${FAVICON_FOLDER}/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${FAVICON_FOLDER}/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${FAVICON_FOLDER}/favicon-16x16.png`}
+        />
         <meta itemProp="image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
         <meta property="og:logo" content="https://chatgpt.kiask.xyz/logo.png"></meta>
         <meta property="og:title" content={title} />
         <meta property="og:image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
-      <meta property="og:image:alt" content="Ultimate ChatGPT Image" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@Vercel" />
-      <meta name="twitter:creator" content="@dom__inic" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
-      <meta property="twitter:image:alt" content="Ultimate ChatGPT Image" />
-      <meta name="twitter:player" content={canonical + "?card=1"} />
-      <meta name="twitter:player:width" content="300" />
-      <meta name="twitter:player:height" content="533" />
+        <meta property="og:image:alt" content="Ultimate ChatGPT Image" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Vercel" />
+        <meta name="twitter:creator" content="@dom__inic" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="https://chatgpt.kiask.xyz/thumbnail.png" />
+        <meta property="twitter:image:alt" content="Ultimate ChatGPT Image" />
+        <meta name="twitter:player" content={canonical + "?card=1"} />
+        <meta name="twitter:player:width" content="300" />
+        <meta name="twitter:player:height" content="533" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
         <script async src="https://www.kiask.xyz/ad-anchor.js" />
